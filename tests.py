82fc14703ca93e32e99197dc347988e708b94e42
John Groton <john.groton@mymail.champlain.edu>
2025-04-11 14:00:23 -0400

    tweaking test

--- tests.py
+++ tests.py
@@ -4,20 +4,20 @@ import server
 
 class TestScreenName(unittest.TestCase):
     def test_invalid_name_1(self):
-        client = Client.ChatClient(True)
+        client = Client.ChatClient(ifTesting=True)
         self.assertFalse(client.check_screen_name("Jimmy Dean"))
 
     def test_invalid_name_2(self):
-        client = Client.ChatClient(True)
+        client = Client.ChatClient(ifTesting=True)
         self.assertFalse(client.check_screen_name(""))
     
     def test_valid_name_1(self):
-        client = Client.ChatClient(True)
+        client = Client.ChatClient(ifTesting=True)
         self.assertTrue(client.check_screen_name("Jimmy_Dean"))
     
     def test_valid_name_2(self):
-        client = Client.ChatClient(True)
-        self.assertTrue(client.check_screen_name("XX_TH3R0CK3R_XX"))
+        client = Client.ChatClient(ifTesting=True)
+        self.assertTrue(client.check_screen_name("XX_K3WLDUD3_XX"))
 
 if __name__ == '__main__':
     unittest.main()
