900e44a6ac250225a16e71b201183d3ab345d047
John Groton <john.groton@mymail.champlain.edu>
2025-04-15 18:34:14 -0400

    more tests

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 import client
 import server
+import socket
 
 test_server = server.ChatServer()
 test_client = client.ChatClient("test")
@@ -19,8 +20,22 @@ class TestScreenName(unittest.TestCase):
         self.assertTrue(test_client.check_screen_name("XX_K3WLDUD3_XX"))
 
 class TestConnection(unittest.TestCase):
-    def test_connection(self):
+    def test_connection_1(self):
         self.assertTrue(test_client.is_connected)
+
+    def test_connection_2(self):
+        HOST = 'localhost'
+        PORT = 1050
+        test_server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        test_client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        test_server.bind(test_server_sock, 'localhost', 1050)
+        self.assertTrue(test_client.connect(test_client_sock, HOST, PORT))
+
+        test_client_sock.close()
+        test_server_sock.close()
+
+        
 
 if __name__ == '__main__':
     unittest.main()
